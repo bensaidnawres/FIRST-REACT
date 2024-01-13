793e971b08e4154cadf6c266e14355de4f61b3a8
bensaidnawres <bensaidnawres030@gmail.com>
2024-01-13 11:44:01 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import BasicExample from './card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+   <BasicExample/>
+   <BasicExample/>
+   <BasicExample/>
+    </div>
+  );
+}
+
+export default App;
